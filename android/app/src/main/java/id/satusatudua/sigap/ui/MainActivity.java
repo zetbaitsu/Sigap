d08bed323cb2f6da3679b6dbf866cf35817fe53d
Zetra <zetra@mail.ugm.ac.id>
2016-01-18 19:27:43 +0700

    Prepare profile page

--- android/app/src/main/java/id/satusatudua/sigap/ui/MainActivity.java
+++ android/app/src/main/java/id/satusatudua/sigap/ui/MainActivity.java
@@ -1,5 +1,6 @@
 package id.satusatudua.sigap.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
@@ -70,7 +71,7 @@ public class MainActivity extends BenihActivity implements TabLayout.OnTabSelect
 
     @OnClick(R.id.button_profile)
     public void openProfile() {
-
+        startActivity(new Intent(this, ProfileActivity.class));
     }
 
     public void setTitle(String title) {
